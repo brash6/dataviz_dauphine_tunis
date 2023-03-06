7fad91800d45d12c3a0caad8a00aaf970c5cbc38
hadmaria <hadrien.mariaccia@liveramp.com>
2023-03-06 11:32:26 +0100

    Update init file

--- TP4/pages/__init__.py
+++ TP4/pages/__init__.py
@@ -1,14 +1,2 @@
-"""Exports the different page components to show in the application"""
-# Examples
-from TP4.pages.example_dashboards import dashboard
-from TP4.pages.example_dashboards.barplot_examples import dashboard
-from TP4.pages.example_dashboards.cardplot_examples import dashboard
-from TP4.pages.example_dashboards.datatable_examples import dashboard
-from TP4.pages.example_dashboards.download_data_button_examples import dashboard
-from TP4.pages.example_dashboards.heatmap_examples import dashboard
-from TP4.pages.example_dashboards.horizontal_barplot_examples import dashboard
-from TP4.pages.example_dashboards.lineplot_examples import dashboard
-from TP4.pages.example_dashboards.notification_popup_examples import dashboard
-from TP4.pages.example_dashboards.scatterplot_examples import dashboard
-from TP4.pages.example_dashboards.switch_button_examples import dashboard
-from TP4.pages.example_dashboards.tabulator_examples import dashboard
+
+
